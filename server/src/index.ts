6bcf35f106227e66e21291751ad9466957e919cc
R T <regantam92@gmail.com>
2026-02-13 07:32:21 +1100

    Added front end fetch api and post methods for submissions

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,19 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 import submissionsRoutes from "./routes/submissionsRoutes.js";
 import buildTeamsRoutes from "./routes/buildTeamsRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.get("/health", (_req, res) => res.json({ ok: true }));
 app.get("/db-test", async (_req, res) => {
